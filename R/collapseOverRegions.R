d22c58e49aa7099429cd12c1f2a90f8cd29dfa6f
ttriche <tim.triche@gmail.com>
2015-10-21 17:30:20 -0700

    rename collapseAtDMRs to collapseOverRegions & keep just an alias

---
+++ R/collapseOverRegions.R
@@ -0,0 +1,17 @@
+#' alias to collapseAtDMRs
+#'
+#' @param x         the SummarizedExperiment-like thing to collapse
+#' @param regions   the regions to collapse over, as a GRanges
+#' @param how       the function used to collapse x over the regions (median)
+#' @param impute    should missing values be imputed first? (TRUE) 
+#' 
+#' @return          an object not unlike x, but collapsed over the regions.
+#' 
+#' @export
+#'
+collapseOverRegions <- function(x, regions, 
+                                how=c("median", "mean", "sum", "max", "min"),
+                                impute=TRUE) {
+  how <- match.arg(how)
+  collapseAtDMRs(x, regions, how, impute)
+}
